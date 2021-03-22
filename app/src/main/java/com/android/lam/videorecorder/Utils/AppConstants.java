fc4e39d6d592c2d5bc1bab2aca1ca35122664554
manish <manishuitburdwan@gmail.com>
2021-03-22 15:03:29 +0530

    * Refactoring

--- app/src/main/java/com/android/lam/videorecorder/Utils/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.android.lam.videorecorder.Utils;
-
-public class AppConstants {
-    public static final int REQUEST_VIDEO_CAPTURE = 1;
-    public static final int PERMISSION_REQUEST = 100;
-}
